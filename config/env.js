4880bd5d88fe231d918b1d8f10c8b8ac693a0f2c
Full-Stack-Atok <komsaykerby@gmail.com>
2025-06-23 18:18:24 +0800

    Day 6: Successfully Created a Reminder Workflow with Upstash and auto reminder calculated.

--- config/env.js
+++ config/env.js
@@ -4,12 +4,15 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 export const {
   PORT,
+  SERVER_URL,
   NODE_ENV,
   DB_URI,
   JWT_SECRET,
   JWT_EXPIRES_IN,
   ARCJET_KEY,
   ARCJET_ENV,
+  QSTASH_TOKEN,
+  QSTASH_URL,
 } = process.env;
 
 console.log(`⚙️  NODE_ENV=${NODE_ENV}, ARCJET_ENV=${ARCJET_ENV}`);
